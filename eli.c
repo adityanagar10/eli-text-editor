4ebc8717f60753c6a5d825375077edf99e6a8817
adityanagar10 <dev.adityanagar@gmail.com>
2025-01-18 15:01:28 +0530

    feat: add ability to read files from system

--- eli.c
+++ eli.c
@@ -1,10 +1,15 @@
 /*** include ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/_types/_ssize_t.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -28,10 +33,17 @@ enum editorKey {
 
 /*** data ***/
 
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
+
 struct editorConfig {
   int cx, cy;
   int screenrows;
   int screencols;
+  int numrows;
+  erow *row;
   struct termios orig_termios;
 };
 
@@ -106,13 +118,13 @@ int editorReadKey() {
         }
       } else {
         switch (seq[1]) {
-        case 'k':
+        case 'A':
           return ARROW_UP;
-        case 'j':
+        case 'B':
           return ARROW_DOWN;
-        case 'l':
+        case 'C':
           return ARROW_RIGHT;
-        case 'h':
+        case 'D':
           return ARROW_LEFT;
         case 'e':
           return END_KEY;
@@ -130,7 +142,18 @@ int editorReadKey() {
     }
     return '\x1b';
   } else {
-    return c;
+    switch (c) {
+    case 'k':
+      return ARROW_UP;
+    case 'j':
+      return ARROW_DOWN;
+    case 'l':
+      return ARROW_RIGHT;
+    case 'h':
+      return ARROW_LEFT;
+    default:
+      return c;
+    }
   }
 }
 
@@ -259,23 +282,30 @@ void abFree(struct abuf *ab) { free(ab->b); }
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3) {
-      char welcome[80];
-      int welcomelen =
-          snprintf(welcome, sizeof(welcome),
-                   "Welcome to Eli editor -- version %s", ELI_VERSION);
-      if (welcomelen > E.screencols)
-        welcomelen = E.screencols;
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
+    if (y >= E.numrows) {
+      if (E.numrows == 0 && y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen =
+            snprintf(welcome, sizeof(welcome),
+                     "Welcome to Eli editor -- version %s", ELI_VERSION);
+        if (welcomelen > E.screencols)
+          welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--)
+          abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+      } else {
         abAppend(ab, "~", 1);
-        padding--;
       }
-      while (padding--)
-        abAppend(ab, " ", 1);
-      abAppend(ab, welcome, welcomelen);
     } else {
-      abAppend(ab, "~", 1);
+      int len = E.row[y].size;
+      if (len > E.screencols)
+        len = E.screencols;
+      abAppend(ab, E.row[y].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3);
@@ -303,19 +333,57 @@ void editorRefreshScreen() {
   abFree(&ab);
 }
 
+/*** row operations ***/
+void editorAppendRow(char *s, size_t len) {
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
+  memcpy(E.row[at].chars, s, len);
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
+}
+
+/*** file i/o ***/
+
+/***
+ * editorOpen will be used for reading files from disk.
+ * */
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp)
+    die("fopen");
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  while ((linelen = getline(&line, &linecap, fp)) != -1) {
+    while (linelen > 0 &&
+           (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+      linelen--;
+    editorAppendRow(line, linelen);
+  }
+  free(line);
+  fclose(fp);
+}
+
 /*** init ***/
 
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.numrows = 0;
+  E.row = NULL;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
     die("getWindowSize");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   enableRawMode();
   initEditor();
-
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
   while (1) {
     char c = '\0';
     editorRefreshScreen();
